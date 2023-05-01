573c635f85685503be6137adb5dfffe6aa09de68
Jackson <jackson1712@hotmail.com>
2023-05-01 20:10:44 -0300

    added file to handler with requisitions http

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Jackson Moura',
-    email: 'jackson@email.com',
-  },
-})
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
