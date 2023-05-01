08c416e68878e696acc642a28085377911db8110
Jackson <jackson1712@hotmail.com>
2023-05-01 18:28:11 -0300

    refactored

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Jackson Moura',
+    email: 'jackson@email.com',
+  },
+})
 
 export const app = fastify()
