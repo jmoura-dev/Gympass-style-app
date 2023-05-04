a4352f854da6f2e17c32f9caf2d370ae20c0bb5f
Jackson <jackson1712@hotmail.com>
2023-05-04 19:04:40 -0300

    added service of checkin

---
+++ src/services/checkin.ts
@@ -0,0 +1,38 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface checkInServiceRequest {
+  userId: string
+  gymId: string
+}
+
+interface checkInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: checkInServiceRequest): Promise<checkInServiceResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDay) {
+      throw new Error()
+    }
+
+    const checkIn = await this.checkInsRepository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
