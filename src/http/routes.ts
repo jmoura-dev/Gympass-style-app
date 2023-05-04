e12c7ab470ec2b27d61733b73c48db5ed1007a2f
Jackson <jackson1712@hotmail.com>
2023-05-04 13:17:08 -0300

    refactored

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { getUserProfile } from './controllers/get-user-profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.post('/users:id', getUserProfile)
 }
