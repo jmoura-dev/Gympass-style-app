54bab7f7005963c56584fa668f44abb7c092a314
Jackson <jackson1712@hotmail.com>
2023-05-08 20:30:32 -0300

    created file to fetch check-ins history

---
+++ src/services/fetch-user-check-inst-history.ts
@@ -0,0 +1,34 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface FetchUserCheckInsHistoryServiceRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryServiceResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryServiceRequest): Promise<FetchUserCheckInsHistoryServiceResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    if (!checkIns) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      checkIns,
+    }
+  }
+}
