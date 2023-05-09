266035978fe06e4f55cc0d029bee9dd3a1fc056a
Jackson <jackson1712@hotmail.com>
2023-05-09 11:43:50 -0300

    created file to validate check-in

---
+++ src/services/validade-check-in.ts
@@ -0,0 +1,33 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ValidateCheckInServiceRequest {
+  CheckInId: string
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    CheckInId,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.findById(CheckInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
