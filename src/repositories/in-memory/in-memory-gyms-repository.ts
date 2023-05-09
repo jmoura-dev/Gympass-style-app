2fa3f50a9474f0210d353220a48ba19b031f4d1c
Jackson <jackson1712@hotmail.com>
2023-05-09 10:41:39 -0300

    refactored: refactored to new function in application

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { Gym, Prisma } from '@prisma/client'
-import { GymsRepository } from '../gyms-repository'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { randomUUID } from 'crypto'
+import { getDistanceBetweenCoordinates } from '@/services/utils/get-distance-between-coordinates'
 
 export class InMemoryGymsInsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -18,6 +19,20 @@ export class InMemoryGymsInsRepository implements GymsRepository {
     this.items.push(gym)
 
     return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10 // distance in km from the user and the gyms
+    })
   }
 
   async searchMany(query: string, page: number) {
