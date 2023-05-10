0b628d3f18ea2a19ccbf3db747af8bb71e0e1b23
Jackson <jackson1712@hotmail.com>
2023-05-10 16:52:23 -0300

    refactored: refactored items to receive new feature of verify jwt

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,17 @@
+import { makeGetUserProfileService } from '@/services/factories/make-get-user-profile'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileService()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
