e3a26a225f82f9929ba272eef79f648dff09d64e
Jackson <jackson1712@hotmail.com>
2023-05-11 17:49:33 -0300

    created test to validate create gyms

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/services/utils/create-and-authenticate-user'
+
+describe('Create Gym e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '3213123123',
+        latitude: -9.6678596,
+        longitude: -35.7516062,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
