ab0c9e23dddf2b38b84c6b8279bcf6d1ee1963ff
Jackson <jackson1712@hotmail.com>
2023-05-11 16:47:28 -0300

    new controller to handler with check-ins

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeCheckInService } from '@/services/factories/make-check-in-service'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+  const { gymId } = createCheckInParamSchema.parse(request.params)
+
+  const createCheckInService = makeCheckInService()
+  await createCheckInService.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send()
+}
