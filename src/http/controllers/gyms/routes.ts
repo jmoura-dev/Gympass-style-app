9f45b8fe863e7992514ea7a7421f05a8a4f5690a
Jackson <jackson1712@hotmail.com>
2023-05-16 16:20:20 -0300

    addded verification in the routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 import { search } from './search'
 import { nearby } from './nearby'
@@ -10,5 +11,6 @@ export async function gymsRoutes(app: FastifyInstance) {
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
-  app.post('/gyms', create)
+
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
